7f998e9e35b634d125b865a039bfee42d7f59f5b
Jason Ergle <jason.ergle@performancematters.com>
2014-05-12 21:46:46 -0400

    Updates to login process

--- src/com/erglesoft/servlet/AuthenticateFilter.java
+++ src/com/erglesoft/servlet/AuthenticateFilter.java
@@ -47,6 +47,7 @@ public class AuthenticateFilter implements Filter {
 					&& !path.startsWith("/assets/") 
 					&& !path.startsWith("/index.jsp")
 					&& !path.startsWith("/index.html")
+					&& !path.startsWith("/jsp/user/createAccount.jsp")
 					&& !path.startsWith("/resetPassword")){
 				if(httpRequest.getSession()==null || UserLoginData.fromHttpSession(httpRequest)==null){
 					System.out.println("No userData on Session, redirecting to landing");
